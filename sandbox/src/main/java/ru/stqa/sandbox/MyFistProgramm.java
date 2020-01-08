8d9dcc8e45caf74edef9bd654ade0f437c2197d8
vilyin24 <56516061+vilyin24@users.noreply.github.com>
2020-01-08 22:14:19 +0300

    Добавленнно пару функций

--- sandbox/src/main/java/ru/stqa/sandbox/MyFistProgramm.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFistProgramm.java
@@ -3,7 +3,22 @@ package ru.stqa.sandbox;
 public class MyFistProgramm {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+
+        System.out.println("Площадь квадрата равна " + area(5,6));
+
     }
 
+    public static void Hello(String somebady) {
+        System.out.println("Hello world" + somebady);
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
+
