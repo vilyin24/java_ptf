88e659e1c1aa8394783166d5394eea3ce12d4097
vilyin24 <56516061+vilyin24@users.noreply.github.com>
2020-01-10 20:10:48 +0300

    сделано 2(4) задание

--- sandbox/src/main/java/ru/stqa/sandbox/MyFistProgramm.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFistProgramm.java
@@ -4,21 +4,14 @@ public class MyFistProgramm {
 
     public static void main(String[] args) {
 
-
-        System.out.println("Площадь квадрата равна " + area(5,6));
+        Point w = new Point();
+        System.out.println("Растояние между двумя точками" +" = " +w.distance(3,4) );
 
     }
 
-    public static void Hello(String somebady) {
-        System.out.println("Hello world" + somebady);
+
     }
 
-    public static double area(double l) {
-        return l * l;
-    }
 
-    public static double area(double a, double b) {
-        return a * b;
-    }
-}
+
 
---
+++ sandbox/src/main/java/ru/stqa/sandbox/Point.java
@@ -0,0 +1,8 @@
+package ru.stqa.sandbox;
+
+public class Point {
+
+  public  double distance(double p1, double p2){
+    return Math.sqrt(p1*p1+p2*p2);
+  }
+}
