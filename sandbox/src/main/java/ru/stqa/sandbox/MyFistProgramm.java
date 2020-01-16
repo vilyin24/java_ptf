87d50d1c8a34d56f3860e549871e33b81ab20a62
vilyin24 <56516061+vilyin24@users.noreply.github.com>
2020-01-16 21:47:56 +0300

    Вот второе задание, пока не знаю как по другому

--- sandbox/src/main/java/ru/stqa/sandbox/MyFistProgramm.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFistProgramm.java
@@ -5,20 +5,22 @@ public class MyFistProgramm {
     public static void main(String[] args) {
         hello("e332");
         double l = 6;
-        System.out.println(areea(l));
-        System.out.println(areea(4,5));
+        Square s = new Square();
+        Rectangle r = new Rectangle();
+        System.out.println(areea(s));
+        System.out.println(areea(r));
     }
 
     public static void hello(String one) {
         System.out.println(one);
     }
 
-    public static double areea(double l) {
-        return l * l;
+    public static double areea(Square s) {
+        return s.l * s.l;
     }
 
-    public static double areea(double a, double b) {
-        return a * b;
+    public static double areea(Rectangle r) {
+        return r.a * r.b;
     }
 }
 
