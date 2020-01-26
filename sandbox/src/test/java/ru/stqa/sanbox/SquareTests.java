c9b8c2077dd483fa34f0c57969d560103aa6318a
vilyin24 <56516061+vilyin24@users.noreply.github.com>
2020-01-26 13:34:52 +0300

    26.01

--- sandbox/src/test/java/ru/stqa/sanbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/sanbox/SquareTests.java
@@ -1,13 +1,14 @@
 package ru.stqa.sanbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.sandbox.Square;
 
 public class SquareTests {
-@Test
+  @Test
   public void testArea(){
-   // Square s = new Square(5);
-    //assert s.area() == 24;
+  Square s = new Square(5);
+    Assert.assertEquals(s.area(),25);
 
   }
 
